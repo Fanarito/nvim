12b0568063c9e18674524038848b906922662e1d
Viktor Sævarsson <viktor@mementopayments.com>
2024-03-25 12:03:32 +0000

    Add toggleterm

--- lua/options.lua
+++ lua/options.lua
@@ -4,21 +4,35 @@ vim.g.maplocalleader = "\\"
 
 local opt = vim.opt
 
-opt.nu = true -- Show line numbers
+opt.nu = true             -- Show line numbers
 opt.relativenumber = true -- Relative line numbers
-opt.shiftround = true -- Round indent
-opt.shiftwidth = 4 -- Size of an indent
-opt.tabstop = 4 -- Number of spaces tabs count for
-opt.scrolloff = 8 -- Keep at least 8 lines loaded from top of screen
+opt.shiftround = true     -- Round indent
+opt.shiftwidth = 4        -- Size of an indent
+opt.tabstop = 4           -- Number of spaces tabs count for
+opt.scrolloff = 8         -- Keep at least 8 lines loaded from top of screen
 opt.completeopt = "menu,menuone,noselect"
 
 opt.swapfile = false -- Disable swap file
 opt.hlsearch = false -- Disable search highlighting
 opt.incsearch = true -- Enable incremental search
 
-opt.wrap = false -- Disable softwrap
+opt.wrap = false     -- Disable softwrap
 
 if vim.fn.has("nvim-0.10") == 1 then
   opt.smoothscroll = true
 end
 
+-- Set keymaps for terminal
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,18 @@
+return {
+	'akinsho/toggleterm.nvim',
+	version = "v2.*",
+	opts = {
+		direction = "float",
+		on_open = function(term)
+			-- Close terminal with ctrl-\
+			vim.api.nvim_buf_set_keymap(
+				term.bufnr,
+				"t", "<c-\\>", "<cmd>ToggleTerm<CR>",
+				{ noremap = true, silent = true }
+			)
+		end,
+	},
+	keys = {
+		{ "<C-\\>", "<cmd>ToggleTerm<cr>", desc = "Toggle terminal" },
+	},
+}
