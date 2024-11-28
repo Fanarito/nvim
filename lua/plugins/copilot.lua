ca293e3a05a3dd7d88ec5041c5cd9613212205c7
Viktor Sævarsson <viktor@vsa.is>
2024-11-28 21:34:47 +0100

    Don't run copilot on env files

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -15,6 +15,15 @@ return {
 			-- },
 		},
 		panel = { enabled = false },
+		filetypes = {
+			sh = function()
+				if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+					-- disable for .env files
+					return false
+				end
+				return true
+			end,
+		}
 	},
 	cond = not vim.g.vscode,
 }
